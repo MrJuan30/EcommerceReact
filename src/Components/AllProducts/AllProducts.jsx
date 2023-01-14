bc26d32740c87fe80cd81e89aa6b9b01547cf6b6
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-14 16:19:49 -0500

    finished functionality on routes, solved some design problems

--- src/Components/AllProducts/AllProducts.jsx
+++ src/Components/AllProducts/AllProducts.jsx
@@ -1,27 +1,36 @@
 import React, {
-    useState,
-    useEffect
+  useState,
+  useEffect
 } from 'react'
 import axios from 'axios'
 import AllProductsCard from './AllProductsCards/AllProductsCard'
 import './AllProducts.css'
+import { useNavigate } from 'react-router-dom'
 
 const AllProducts = () => {
-    const [AllProducts, setAllProducts] = useState()
-    const URL = 'https://fakestoreapi.com/products?limit=19'
-    useEffect(() => {
-        axios
-            .get(URL)
-            .then((res) => setAllProducts(res.data))
-            .catch((err) => console.log(err))
-    }, [])
+  const navigate = useNavigate()
+
+  const HandleBack = () => {
+    navigate(`/`)
+  }
+  const [AllProducts, setAllProducts] = useState()
+  const URL = 'https://fakestoreapi.com/products?limit=19'
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => setAllProducts(res.data))
+      .catch((err) => console.log(err))
+  }, [])
   return (
     <div>
-        <div className='AllProducts_Container'>
-        {AllProducts?.map((allproducts) =>
-                    <AllProductsCard allproducts={allproducts} key={allproducts.id} />
-                )}
+      <div className='AllProducts_Container'>
+        <div className='Btn_Back'>
+          <box-icon name='chevron-left' size='4rem' onClick={HandleBack}></box-icon>
         </div>
+        {AllProducts?.map((allproducts) =>
+          <AllProductsCard allproducts={allproducts} key={allproducts.id} />
+        )}
+      </div>
     </div>
   )
 }
--- src/Components/ProductsById/ProductsById.jsx
+++ src/Components/ProductsById/ProductsById.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import './ProductsById.css'
 
 
 const ProductsById = () => {
@@ -20,11 +22,35 @@ const ProductsById = () => {
       });
   }, [id]);
 
-  console.log(ProductById)
+  const navigate = useNavigate()
 
+  const HandleBack = () => {
+    navigate(`/products`)
+  }
   return (
-    <div>
-      <h1>{ProductById?.title}</h1>
+    <div className='ProductById'>
+      <div className='ProductById_Container'>
+        <div className="ProductById_Header">
+          <div className='Btn_Back Btn_Back_2'>
+            <box-icon name='chevron-left' size='4rem' onClick={HandleBack}></box-icon>
+          </div>
+          <h3 className='ProductById_Title'>{ProductById?.title}</h3>
+        </div>
+        <div className='ProductById-Image_Container'>
+          <img src={ProductById?.image} alt={ProductById?.title} className="ProductById_Image" />
+        </div>
+        <div className='ProductById_Info'>
+          <p className="ProductById_Description">
+            {ProductById?.description}
+          </p>
+        </div>
+        <div className="ProductById_Footer">
+          <p className='ProductById_Price-Text'>$ {ProductById?.price}</p>
+          <box-icon name='cart-add' size='2rem'></box-icon>
+
+        </div>
+      </div>
+
     </div>
   )
 }
